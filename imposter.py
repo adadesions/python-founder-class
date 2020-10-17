7fb2455251457d2211dd866fa1722d112a009e21
adadesions <adadesions@gmail.com>
2020-10-17 18:26:25 +0700

    3rd

--- imposter.py
+++ imposter.py
@@ -1,5 +1,21 @@
 import random
 
+missons = [
+    "Go to coding room",
+    "Fix electricity",
+    "Solve problem at meeting room",
+    "Fix the generator",
+    "Switch A to B"
+]
+
+
+def generate_player(name, missons, isImposter):
+    return {
+        'name': name,
+        'missons': missons,
+        'isImposter': isImposter
+    }
+
 
 if __name__ == '__main__':
     num_player = int(input("Enter number of players: "))
@@ -18,7 +34,13 @@ if __name__ == '__main__':
         # 0 = Crewmate, 1 = imposter
         players[imposter_index] = 1
 
-    # Show player list
-    print(players)
-    print("Imposter amount:", len(imposters))
+    players_info = []
+    for idx, p in enumerate(players):
+        misson_index = random.randint(0, len(missons) - 1)
+        isImposter = (1 == p)
+        player = generate_player(idx, missons[misson_index], isImposter)
+
+        players_info.append(player)
+    
+    print(players_info)
 
